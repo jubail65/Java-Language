d13d49eed1a6c8cf4f5ef89a3042579d6665b891
Jubail Islam <islam35-1048@diu.edu.bd>
2023-12-27 01:03:09 +0600

    How to print word in java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// Java hello world print. How to print a world (like "Hello world")
+public class Main {
+    public static void main(String []args){
+        // print hello world
+        System.out.println("Hello word");
+    }
+}
